68f87380748e2fe8685bfff2cf52cc8c23d0f552
unknown <alejandrojzeballos@gmail.com>
2022-02-03 13:20:32 -0400

    {add} Introduccion a react

--- apuntes.js
+++ apuntes.js
@@ -1,4 +1,25 @@
 /* 
 Inicio Proyecto
+Arquitectura basada en componentes, en unidades que se intercomunicara para crear funcionalidades
+
+extension de archivo hace referencia a xml y js hace referencia a motores de plantillas.
+transforma al codigo pertinente.
+
+DOM vs virtual dom
+Es una representacion del dom real cunado pase algun cambio el virtual dom se compra con el real dom asi los cambios solo se realizan en lugares especificos de la app y no renderizas toda la pagina.
+
+Ciclo de vida
+todas las app, componetes y los que creamos tienen un inicio y un final que afectan al virtual dom. asi podemos identificar nuestros eventos con elementos claves
+
+estado
+piexa de codigo que mantiene la comunicacion entre componentes y como los comonentes van interactuando.
+
+eventos
+Clicks, activar la pagina, overs con el raton.
+
+hooks
+Se usan todos estos recursos, se usan mejor para trabajar con los componentes. con un hook podemos crear cada uno de estos elemntos previos.
+
+
 
 */
