f02148858b64f4b01f923b2129869d3ec0a3d946
unknown <alejandrojzeballos@gmail.com>
2022-02-18 19:51:09 -0400

    {fix} case sensitive error en productList en el import de productitem

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProductItem from '../components/ProductItem';
+import ProductItem from '../components/Productitem';
 import '../styles/ProductList.scss';
 import useGetProducts from '../hooks/useGetProducts';
 
