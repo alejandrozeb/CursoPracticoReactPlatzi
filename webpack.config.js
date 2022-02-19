0b5f83851985c23efa2972d8262486705caa8163
unknown <alejandrojzeballos@gmail.com>
2022-02-18 20:18:46 -0400

    {fix} package json homepage, webpackconfig añadiendo path con nombre de repo y en routes añadiendo nuevo basename en browserrouter

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath:'/'
+    filename: "CursoPracticoReactPlatzi/bundle.js",
+    publicPath:'/CursoPracticoReactPlatzi/'
   },
   mode: "development",
   resolve: {
